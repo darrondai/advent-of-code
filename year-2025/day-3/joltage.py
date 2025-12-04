d87c1d1659a5ef8187c231d9666f3365cc8e9199
Darron Dai <daidarron@gmail.com>
2025-12-03 19:34:01 -0800

    Complete day 3 part 2, need to clean up comments and logic

--- year-2025/day-3/joltage.py
+++ year-2025/day-3/joltage.py
@@ -27,7 +27,7 @@ def accumulate_max_voltages(battery_banks: list[str]) -> int:
     return total_voltage
 
 
-def smart(digits: str) -> int:
+def smart(digits: str, num_digits: int) -> int:
     # better way is to use a monotonic nonincreasing deque?
     # OR just use two pointers
     # but i will use deque just in case part 2 fucks me (3 digits+)
@@ -88,11 +88,127 @@ def smart(digits: str) -> int:
     return 0
 
 
-def accumulate_max_voltages_part_2(battery_banks: list[str]) -> int:
+# the basic idea is to take the first n digits from the right
+# once we have the first n digits, we can start deciding what to do with the incoming numbers from the left side
+# essentially at time we add a digit, we want to make sure that once the digits shift over, that the new starting digit is greater or equal to the previous
+# the rest of the digits behind it dont really matter? THEY DO.....
+
+# track the thing we want to remove (they are save to remove once we find an element greater than the left side head?)
+# deque plus stack?
+# fill deque initially
+# once it is full, decide whether or not to add to it by checking if the new element is greater than the left most element in the deque
+# if it is, we then remove elements from the dq into the stack (for sure taken if there an)
+# thus rightmost element in the dq is the element that we pop and decide if it stays valuable or not
+
+# at the end we just pop from the deque until empty, then pop from the stack until we have enough digits
+# the deque should be monotonic decreasing from left to right i.e. [8, 8, 3, 2, 1]
+# the stack should be
+
+# if the next element is lesser than the top of the dq, process the dq basically, and push the digits to the stack only if they are greater than the stack top
+
+# maybe the deque gets filled to the max first (may not necessarily be monotonic)
+# then at the end, we do a final processing that pops the smaller of the dq.right and stack.top
+
+# stack is monotonic non decreasing left to right?
+# whenever we want to insert into it, pop from top until the inserted element is <= stack.top
+
+# the deque should always stay at max capacity, but when we see an element we want to add (aka greater than deque.left), popright and then process into the stack
+
+
+# at the end, pop from dq left and process into stack until the combined dq and stack length = digits
+def scuffed(digits: str, num_digits: int) -> int:
+    queue = deque()
+    # represents elements that are guaranteed to be taken
+    dq = deque()
+
+    # for digit in digits:
+    #     if not queue or queue[0] <= digit:
+    #         queue.append(digit)
+
+    # if too big, pop and try adding to stack
+    #
+
+    # wait... does a single monotonic increasing deque work?
+    # we want to replace the right most element that we are greater than
+    # no it breaks when the window is too big i.e 5 digits "599411816"
+    # we solve this by only popping from stack while dq + stack len is greater than num_digits (though we only pop from stack while dq.left > stack.top)
+    # the stack is monotonically increasing at all times
+    # the deque is monotonically increasing at most times, except for
+
+    # lets say stack always is monotonically increasing
+
+    # do we just pop the smaller of queue.right and stack.left?
+    # no. instead, just insert into stack when queue.right is >= stack
+    # and when stack is too big, pop from the end of it??? so its a deque
+    # rather than popping from the stack, pop from the queue
+    # we basically want the stack to always be taken from that point on
+    # queue may not be monotonic, but we can ensure that the elements that we insert are
+
+    # so...
+    # regular queue and a monotonic nondecreasing deque (insert if >= leftmost element)
+    # the queue is just to contain all of the digits that are being held back by a single digit
+    # elements get inserted into the queue, and once it hits capacity, we keep popping the left most element and trying to add insert it into the stack, stopping once we find another element that is smaller than the stack top
+    for digit in digits:
+        # if len(queue) + len(dq) > num_digits:
+        #     queue.append(digit)
+        queue.append(digit)
+
+        # while queue isnt empty, insert as many elements from queue into the guaranteed dq
+        while queue and (not dq or queue[0] >= dq[-1]):
+            dq.append(queue.popleft())
+            # if dq is too big, popleft from it (removing smallest element)
+            # if len(dq) >
+
+        # once we know that we have enough digits to construct a number, we can start doing interesting things (treating insertion like a monotonic non decreasing queue)
+        # if the number is smaller than the first digit in the queue, ignore it
+
+    # final processing of queue elements
+    # read left to right (queue + stack) and cast into a integer
+    return 0
+
+
+# we want to ensure that we only remove the smallest digit possible
+# within the digits we already have in the dq, we want to remove the first element that is smaller than
+
+
+# intuition is that we want to get rid of the left most digit that is smaller than its right neighbor (guarantees an increase)
+# the neighbors should update after removal
+# basically we go through the string repeated remove the left most digit that is smaller than its right neighbor
+
+# what if we just started from the left side and used a monotonic nonincreasing stack? EERRRR pop from stack until the stack[-1] >= curr digit
+# BUT we want to make sure that we have enought digits to make a number, so once we have popped the max amount of numbers (total batteries in bank = batteries popped + batteries in stack + batteries left) we just default to the stack + remaining batteries
+
+
+def calc_max_voltage_part_2(battery_bank: str, num_digits: int) -> int:
+    max_voltage = []
+    batteries_left = len(battery_bank)
+    for digit in battery_bank:
+        while max_voltage and digit > max_voltage[-1] and batteries_left > num_digits:
+            max_voltage.pop()
+            batteries_left -= 1
+        if len(max_voltage) == num_digits:
+            batteries_left -= 1
+            continue
+        max_voltage.append(digit)
+        print("".join(max_voltage))
+
+    # 4589185, 4 digits
+    # 4 -> 5 -> 8 -> 9 -> 9185
+    # 3826134, 4 digits
+    # 3 -> 8 -> 82 -> 86 -> 861 -> 8634
+    # 123123123, 2 digits
+    # 1 -> 2 -> 3 -> 31 -> 32 -> 33 -> 33 -> 33 -> 333????
+    # edge case of thing being too full, solve by just taking the not adding if the voltage is full
+
+    # max_voltage.extend()
+    return int("".join(max_voltage))
+
+
+def accumulate_max_voltages_part_2(battery_banks: list[str], num_digits: int) -> int:
     total_voltage = 0
 
     for bank in battery_banks:
-        total_voltage += smart(bank)
+        total_voltage += calc_max_voltage_part_2(bank, num_digits)
 
     return total_voltage
 
--- year-2025/day-3/tests.py
+++ year-2025/day-3/tests.py
@@ -1,4 +1,10 @@
-from joltage import accumulate_max_voltages, accumulate_max_voltages_part_2
+from joltage import (
+    accumulate_max_voltages,
+    accumulate_max_voltages_part_2,
+    naive,
+    smart,
+    calc_max_voltage_part_2,
+)
 
 
 def test_example_part_1():
@@ -18,4 +24,22 @@ def test_example_part_2():
         "234234234234278",
         "818181911112111",
     ]
-    assert accumulate_max_voltages_part_2(battery_banks) == 3121910778619
+
+    expected_voltages = [987654321111, 811111111119, 434234234278, 888911112111]
+
+    for bank, expected in zip(battery_banks, expected_voltages):
+        assert calc_max_voltage_part_2(bank, 12) == expected
+
+    assert accumulate_max_voltages_part_2(battery_banks, 12) == 3121910778619
+
+
+def test_battery_logic():
+    battery_bank = "123123123"
+    num_digits = 2
+    assert calc_max_voltage_part_2(battery_bank, num_digits) == 33
+
+    battery_bank = "123123123"
+    # queue: ... [2,3,1,2,3] -> [2,3,1,2,3] -> [3,3,1,2,3]
+    # stack: ... [] -> [] -> [3] -> [3] -> [3] -> [3]
+    num_digits = 5
+    assert calc_max_voltage_part_2(battery_bank, num_digits) == 33123
